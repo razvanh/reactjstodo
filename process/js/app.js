3d45235fbefea23f787497ecd630d33a7e2e6c09
Razvan <razvan.horeanga@gmail.com>
2017-02-16 22:24:15 -0600

    moved dummy to do items in external json file

--- process/js/app.js
+++ process/js/app.js
@@ -4,29 +4,31 @@ var ReactDOM = require('react-dom');
 var MainInterface = React.createClass({
 	getInitialState: function (){
 		return {
-			data: [
-			{
-				"title": "Create To Do App using React.js",
-				"dueDate": "2017-03-10 08:00",
-				"details": "Implement simple To Do list functionality using React.js"
-			},
-			{
-				"title": "Work on the design of the To Do app",
-				"dueDate": "2015-04-10 08:00",
-				"details": "Make it beautiful:)"
-			},
-			]
-		}
+			myToDos: []
+		}//return
 	},//getInitialState
 
+	componentDidMount: function(){
+		this.serverRequest =  $.get('./js/data.json', function(result){
+			console.log('test');
+			var tempToDos = result;
+			this.setState({
+				myToDos: tempToDos
+			});//setState
+		}.bind(this));//serverRequest
+	},
+
+	componentWillUnmount: function(){
+		this.serverRequest.abort();
+	},
 	render: function(){
-		var todos = this.state.data;
+		var todos = this.state.myToDos;
 		todos = todos.map(function(item,index){
 			return (
 					<li key={index}>
-						<h2>{ this.state.data[index].title }</h2>
-						<p className="small-text">Due on { this.state.data[index].dueDate}</p>
-						<p>{ this.state.data[index].details }</p>
+						<h2>{ this.state.myToDos[index].title }</h2>
+						<p className="small-text">Due on { this.state.myToDos[index].dueDate}</p>
+						<p>{ this.state.myToDos[index].details }</p>
 					</li>
 				)//return
 		}.bind(this)); //todos.map
